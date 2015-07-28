056fbf6c37e62c69d501ab8b1dcd5faae06f7ddf
Ed George <esm@hotmail.co.uk>
2015-07-29 00:52:44 +0100

    Add Parse to project

--- app/src/main/java/uk/co/edgeorgedev/streamernetwork/StreamerNetworkApplication.java
+++ app/src/main/java/uk/co/edgeorgedev/streamernetwork/StreamerNetworkApplication.java
@@ -3,6 +3,13 @@ package uk.co.edgeorgedev.streamernetwork;
 import android.app.Application;
 import android.content.Context;
 
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by edgeorge on 26/07/15.
  */
@@ -14,5 +21,20 @@ public class StreamerNetworkApplication extends Application {
     public void onCreate() {
         super.onCreate();
         staticContext = getApplicationContext();
+
+        try {
+            Properties prop = new Properties();
+            InputStream input = getBaseContext().getAssets().open("parse.properties");
+
+            prop.load(input);
+
+            Parse.initialize(this, prop.getProperty("APPLICATION_ID"), prop.getProperty("CLIENT_ID"));
+            ParseInstallation.getCurrentInstallation().saveInBackground();
+
+            input.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
