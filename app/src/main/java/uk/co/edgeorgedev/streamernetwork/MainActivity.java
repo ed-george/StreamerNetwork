fd6c97cc8d3a40fbabc526b045b888ae04596bca
Ed George <ed.george@himumsaiddad.com>
2015-07-30 18:06:10 +0100

    Remove menu options

--- app/src/main/java/uk/co/edgeorgedev/streamernetwork/MainActivity.java
+++ app/src/main/java/uk/co/edgeorgedev/streamernetwork/MainActivity.java
@@ -54,27 +54,7 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerF
         getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new NetworkFeedFragment()).commit();
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
